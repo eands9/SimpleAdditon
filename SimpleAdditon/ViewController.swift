f26740d17643e3de77c72518c3312eb4e7a1466e
Kyle Hernandez <kylehernandez@kyles-macbook-air.local>
2018-12-17 19:46:14 -0600

    add show button

--- SimpleAdditon/ViewController.swift
+++ SimpleAdditon/ViewController.swift
@@ -13,18 +13,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var questionLbl: UILabel!
     @IBOutlet weak var userEnteredTxt: UITextField!
     
-    var numA = Int.random(in: 100 ... 1000)
-    var numB = Int.random(in: 100 ... 1000)
+    var numA = 0
+    var numB = 0
     var correctAnswer : Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLbl.text = "\(String(numA)) + \(String(numB))"
+        nextQuestion()
     }
 
     @IBAction func checkBtn(_ sender: Any) {
         if userEnteredTxt.text == String(correctAnswer){
             print("You are so smart!")
+            nextQuestion()
         }
         else {
             print("Try Again!")
@@ -37,6 +38,14 @@ class ViewController: UIViewController {
         correctAnswer = numA + numB
     }
     func nextQuestion() {
+       numA = Int.random(in: 100 ... 1000)
+       numB = Int.random(in: 100 ... 1000)
+       questionLbl.text = "\(String(numA)) + \(String(numB))"
+    
+    }
+    @IBAction func showBtn(_ sender: Any) {
+        getAnswer()
+        userEnteredTxt.text = String(correctAnswer)
         
     }
 }
