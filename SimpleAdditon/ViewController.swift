61e52d9e1568da9d4baf1d806e16aa6d82b4b693
Kyle Hernandez <kylehernandez@kyles-macbook-air.local>
2018-12-17 18:57:07 -0600

    clean up

--- SimpleAdditon/ViewController.swift
+++ SimpleAdditon/ViewController.swift
@@ -16,49 +16,28 @@ class ViewController: UIViewController {
     var numA = Int.random(in: 100 ... 1000)
     var numB = Int.random(in: 100 ... 1000)
     var correctAnswer : Int = 0
-
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
         questionLbl.text = "\(String(numA)) + \(String(numB))"
-        
-        
-
     }
 
     @IBAction func checkBtn(_ sender: Any) {
         if userEnteredTxt.text == String(correctAnswer){
-            print("U are so smart!")
+            print("You are so smart!")
         }
         else {
             print("Try Again!")
-            
         }
-        
         getAnswer()
-        print(correctAnswer)
-        
         userEnteredTxt.text = ""
-    
-        
-        
     }
-    
     
     func getAnswer() {
         correctAnswer = numA + numB
-        
-      
-        
-        
-       
-        
+    }
+    func nextQuestion() {
         
     }
-    
-
-
 }
 
